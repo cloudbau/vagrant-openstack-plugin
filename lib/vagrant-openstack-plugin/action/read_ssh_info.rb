e98f3ca2f83b44240dea98a3234f736595c9ec6f
Joe Topjian <joe@topjian.net>
2013-09-10 12:41:33 -0600

    Assistance with the network name
    This patch provides assistance for identifying and, in some cases,
    setting the network name. It adds some debug lines so the user can
    easily identify what network names are available to their tenant. If the
    tenant only has access to one network, that network name will be used.
    
    This should resolve issues such as #16.

--- lib/vagrant-openstack-plugin/action/read_ssh_info.rb
+++ lib/vagrant-openstack-plugin/action/read_ssh_info.rb
@@ -31,11 +31,39 @@ module VagrantPlugins
 
           config = machine.provider_config
 
-          if config.network
-            host = server.addresses[config.network].last['addr'] rescue nil
-          else
-            host = server.addresses[config.address_id].last['addr'] rescue nil
+          # Print the server network name(s)
+          server.addresses.each do |network_name, network_info|
+            @logger.debug("OpenStack Network Name: #{network_name}")
           end
+
+          # See if either config.network or config.address_id is set
+          # in order to get the instance's IP address.
+          #
+          # If neither are set, try to determine the network name automatically
+          if config.network
+            if server.addresses.has_key?(config.network)
+              host = server.addresses[config.network].last['addr']
+            end
+          elsif config.address_id
+            if server.addresses.has_key?(config.address_id)
+              host = server.addresses[config.address_id].last['addr']
+            end
+          end
+
+          if host.nil?
+            @logger.debug("Neither config.network or config.address_id were used")
+            if server.addresses.length == 1
+              network_name = server.addresses.keys[0]
+              @logger.debug("Using #{network_name} automatically")
+              host = server.addresses[network_name].last['addr']
+            end
+          end
+
+          # If host got this far and is still nil/empty, raise an error or
+          # else vagrant will try connecting to localhost which will never
+          # make sense in this scenario
+          raise Errors::SSHNoValidHost if host.nil? or host.empty?
+
           # Read the DNS info
           return {
             # Usually there should only be one public IP
