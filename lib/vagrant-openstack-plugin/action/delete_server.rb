0f99ca5511bb7b5de05f4e5a808a863dca66c9ab
Micheal Jones <micheal.jones@cybera.ca>
2014-11-28 11:57:58 -0700

    Don't release a floating IP unless it was automatically assigned.

--- lib/vagrant-openstack-plugin/action/delete_server.rb
+++ lib/vagrant-openstack-plugin/action/delete_server.rb
@@ -24,7 +24,7 @@ module VagrantPlugins
             if server
               ip = server.floating_ip_address
               server.destroy
-              if machine.provider_config.floating_ip_pool
+              if machine.provider_config.floating_ip_pool && machine.provider_config.floating_ip == "auto"
                 address = env[:openstack_compute].list_all_addresses.body["floating_ips"].find{|i| i["ip"] == ip}
                 if address
                   env[:openstack_compute].release_address(address["id"])
